32fbdbe20a622e0380d3df4e1646535688552c66
risgpta <risgpta@gmail.com>
2021-05-27 00:42:33 +0530

    port

--- index.js
+++ index.js
@@ -49,7 +49,9 @@ mongoose
   .catch((err) => console.log(err));
 
 //listen
-app.listen(5000, () => console.log(`Server running on port 5000`));
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
 
 //Middlewares
 /*
